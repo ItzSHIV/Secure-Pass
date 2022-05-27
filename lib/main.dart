c62fd15c347e996a624c3e2ee28df50c434a2235
unknown <rubenfalcao14@gmail.com>
2022-05-27 17:08:41 +0530

    Step-2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:secure_pass/firebase_options.dart';
 import 'package:secure_pass/views/login_view.dart';
+import 'package:secure_pass/views/register_view.dart';
+import 'package:secure_pass/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +15,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -22,53 +28,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home'),
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState){
             case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text('Done');
-            // } else{
-            //   return const VerifyEmailView();
-            // }     
-            return const LoginView();  
+            final user = FirebaseAuth.instance.currentUser;
+            if(user != null){
+              if(user.emailVerified){
+                print('Email is verified');
+              } else{
+                  return const VerifyEmailView();
+              } 
+            } else{
+              return const LoginView();
+            }
+            return const Text('Done');
             default:
-              return const Text('Loading...');
+              return const CircularProgressIndicator();
           }  
         },
-      ),
-    );
-  }
-}
-
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({ Key? key }) : super(key: key);
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Please verify your email address: '),
-          TextButton(
-          onPressed: (){
-              final user = FirebaseAuth.instance.currentUser;
-              user?.sendEmailVerification();
-          }, 
-        child: const Text('Send email verification'),
-          )
-      ],
-    );
+      );
   }
 }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -28,47 +28,61 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Enter your email',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Column(
+            children: [
+              TextField(
+                controller: _email,
+                enableSuggestions: false,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
               ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: 'Enter your password',
+              TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your password',
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try{
-                  final UserCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: email, 
-                  password: password,
-                );
-                print(UserCredential);
-                } on FirebaseAuthException catch (e){
-                  if (e.code == 'user-not-found') {
-                    print('User not found');
-                  } else if(e.code == 'wrong-password'){
-                    print('Wrong password');
+              TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+                  try{
+                    final UserCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email, 
+                    password: password,
+                  );
+                  print(UserCredential);
+                  } on FirebaseAuthException catch (e){
+                    if (e.code == 'user-not-found') {
+                      print('User not found');
+                    } else if(e.code == 'wrong-password'){
+                      print('Wrong password');
+                    }
                   }
-                }
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        );
+                },
+                child: const Text('Login'),
+              ),
+              TextButton(
+                onPressed: (){
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/register/',
+                     (route) => false,
+                  );
+                },
+                child: const Text('Not Registered ? Register here!'),
+              )
+            ],
+          ),
+    );
   }
 }
