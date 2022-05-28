4f60745df780f77379d355140cfd6e87c6b537fd
unknown <rubenfalcao14@gmail.com>
2022-05-28 12:59:45 +0530

    Step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:secure_pass/firebase_options.dart';
 import 'package:secure_pass/views/login_view.dart';
 import 'package:secure_pass/views/register_view.dart';
 import 'package:secure_pass/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +18,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/password/':(context) => const PasswordView()
       },
     ),
   );
